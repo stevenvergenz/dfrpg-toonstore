218f4affc96071e41833c13661fd0de454af60ba
Steven Vergenz <vergenzs@gmail.com>
2013-06-24 14:48:39 -0400

    Converted request parsing to express.js

--- app.js
+++ app.js
@@ -1,5 +1,57 @@
+var https = require('https');
+var fs = require('fs');
+var libpath = require('path');
+var liburl = require('url');
+var express = require('express');
 
-var server = require('./server.js');
+var global = require('./global.js');
+var register = require('./register.js');
+var login = require('./login.js');
 
-server.startServer();
+
+var app = express();
+app.use(express.bodyParser());
+
+app.use( function(req,res,next){
+	global.log(req.method, req.url);
+	next();
+});
+
+app.get('/register', register.registrationPage);
+app.post('/register', register.register);
+app.get('/register/verify', register.checkUsername);
+
+//app.get('/login', login.handleRequest);
+
+app.use(function(req,res)
+{
+	var url = liburl.parse(req.url, true);
+
+	// convert url to fs path
+	var path = 'public' + url.pathname;
+	if( url.pathname == '/' )
+		path += 'index.html';
+	path = libpath.normalize(path);
+
+	// serve files from the public dir
+	if( fs.existsSync(path) && fs.statSync(path).isFile() ){
+		global.log('Serving', path);
+		res.sendfile(path);
+	}
+	
+	// return 404
+	else {
+		global.error('File not found:', path, global.logLevels.warning);
+		res.send(404, '404 Not Found');
+	}
+
+});
+
+
+var options = {
+	key: fs.readFileSync( global.config.ssl_info.key ),
+	cert: fs.readFileSync( global.config.ssl_info.cert )
+};
+https.createServer(options, app).listen(3001);
+global.log('Server running at http://localhost:3001/');
 
--- register.js
+++ register.js
@@ -1,89 +1,68 @@
 var fs = require('fs');
 var libpath = require('path');
-var liburl = require('url');
 var mysql = require('mysql');
 var crypto = require('crypto');
 
 var global = require('./global.js');
 
 
-function handleRequest(request, response)
+function registrationPage(req, res)
 {
-	var url = liburl.parse(request.url, true);
-
-	// root /register page
-	if( url.pathname == '/register' && request.method == 'GET' )
-	{
-		var path = libpath.normalize('public/register.html');
-
-		global.log('Serving registration file:', path);
-		fs.readFile( path, function(err,data){
-			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.end(data);
-		});
-	}
-
-	// registration request
-	else if( url.pathname == '/register' && request.method == 'POST' )
-	{
-		var body = '';
-		request.on('data', function(chunk){
-			body += chunk;
-		});
-
-		request.on('end', function()
-		{
-			body = global.parsePostData(body);
-
-			// salt and hash the password
-			body.salt = crypto.randomBytes(32);
-			body.password = crypto.pbkdf2Sync(body.password, body.salt, 1000, 32);
-			body.salt = body.salt.toString('hex');
-			body.password = body.password.toString('hex');
-			global.log('Registering user:', body);
-
-			// connect to the db
-			var connection = mysql.createConnection( global.config.database );
-			connection.query(
-				'INSERT INTO Users SET username = ?, email = ?, salt = UNHEX(?), password = UNHEX(?), registered = DEFAULT, last_login = DEFAULT;', 
-				[body.username,body.email,body.salt,body.password],
-				function(err, rows, fields){
-					if( err ){
-						global.log('Registration error:', err);
-						response.writeHead(500);
-						response.end();
-					}
-					else {
-						global.log('Registration successful');
-						global.redirect('/post-register.html', response);
-					}
-				}
-			);
-		});
-	}
-
-	// username testing
-	else if( url.pathname == '/register/verify' )
-	{
-		var user = url.query.a;
-		
-		// connect to the db
-		var connection = mysql.createConnection( global.config.database );
-		connection.query('SELECT COUNT(username) AS userCount FROM Users WHERE username = ?', [user], function(err, rows, fields)
-		{
-			if( err ){
-				global.error('MySQL error:', err);
-				response.writeHead(500);
-				response.end('MySQL error');
-			}
-			else {
-				var userFound = rows[0]["userCount"] == 1;
-				response.writeHead(200, {'Content-Type': 'application/json'});
-				response.end(JSON.stringify({found: userFound}));
-			}
-		});
-	}
+	var path = libpath.normalize('public/register.html');
+	global.log('Serving registration file:', path);
+	res.sendfile( path );
 }
 
-exports.handleRequest = handleRequest;
+function register(req,res)
+{
+	var body = req.body;
+
+	// salt and hash the password
+	body.salt = crypto.randomBytes(32);
+	body.password = crypto.pbkdf2Sync(body.password, body.salt, 1000, 32);
+	body.salt = body.salt.toString('hex');
+	body.password = body.password.toString('hex');
+	global.log('Registering user:', body);
+
+	// connect to the db
+	var connection = mysql.createConnection( global.config.database );
+	connection.query(
+		'INSERT INTO Users SET username = ?, email = ?, salt = UNHEX(?), password = UNHEX(?), registered = DEFAULT, last_login = DEFAULT;', 
+		[body.username,body.email,body.salt,body.password],
+		function(err, rows, fields){
+			if( err ){
+				global.error('Registration error:', err, global.logLevels.error);
+				res.status(500).end();
+			}
+			else {
+				global.log('Registration successful');
+				res.redirect('/post-register.html');
+			}
+		}
+	);
+}
+
+// username testing
+function checkUsername(req,res)
+{
+	var user = req.query.a;
+	
+	// connect to the db
+	var connection = mysql.createConnection( global.config.database );
+	connection.query('SELECT COUNT(username) AS userCount FROM Users WHERE username = ?', [user], function(err, rows, fields)
+	{
+		if( err ){
+			global.error('MySQL error:', err);
+			res.send(500, 'MySQL error');
+		}
+		else {
+			var userFound = rows[0]["userCount"] == 1;
+			res.json(200, {found: userFound});
+		}
+	});
+}
+
+exports.registrationPage = registrationPage;
+exports.register = register;
+exports.checkUsername = checkUsername;
 
