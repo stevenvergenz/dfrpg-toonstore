0871c6b1bba4c257b7ffeb25b8915255257bd961
Steven Vergenz <vergenzs@gmail.com>
2013-06-25 17:51:21 -0400

    Added character creation logic

--- newtoon.js
+++ newtoon.js
@@ -6,16 +6,37 @@ var global = require('./global.js');
 function newCharacterPage(req,res)
 {
 	if( req.session.user ){
-		req.render('newtoon');
+		global.log('Serving new character page');
+		var params = {page: req.url, logged_user: req.session.user};
+		res.render('newtoon', params);
 	}
 	else {
-		req.redirect('/login');
+		res.redirect('/login?redir=/newtoon');
 	}
 }
 
 function newCharacterRequest(req,res)
 {
-	
+	global.log('Attempting character creation');
+	var connection = mysql.createConnection( global.config.database );
+	connection.query('INSERT INTO Characters SET created_on=NOW(), ?;',
+		{'canonical_name': req.body.canon_name, 'name': req.body.name,
+			'owner': req.session.user, 'concept': req.body.concept},
+		function(err,rows,fields)
+		{
+			if( err ){
+				global.error('MySQL error:', err);
+				var params = {page: req.url, logged_user: req.session.user, 
+					message: 'You are already using that short name'};
+				res.render('newtoon', params);
+			}
+			else {
+				global.log('Creation successful');
+				var url = '/'+req.session.user+'/'+req.body.canon_name;
+				res.redirect(url);
+			}
+		}
+	);
 }
 
 exports.newCharacterPage = newCharacterPage;
