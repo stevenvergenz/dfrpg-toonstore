c38a55b1849187ad67e500df77defb8f2abf1615
Steven Vergenz <vergenzs@gmail.com>
2013-06-27 08:36:22 -0400

    Finished skills section, file cleanup

--- commit-file.js
+++ commit-file.js
@@ -9,14 +9,15 @@ var filename = process.argv[2];
 var file = JSON.parse(fs.readFileSync(filename));
 
 var connection = mysql.createConnection( global.config.database );
-connection.query('UPDATE Characters SET info = ? WHERE canonical_name = "dresden";', [JSON.stringify(file)],
+connection.query('UPDATE Characters SET info = ? WHERE canonical_name = "lifeson";', [JSON.stringify(file)],
 	function(err,rows,fields){
 		if(err){
 			console.log(err);
 		}
 		else {
+			console.log(rows.message);
 			console.log('Success');
 		}
+		connection.end();
 	}
 );
-connection.end();
