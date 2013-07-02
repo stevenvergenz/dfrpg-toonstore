f7c108199177744572cc650bcf1ff1336d1b0ab6
Steven Vergenz <vergenzs@gmail.com>
2013-07-01 23:57:48 -0400

    Added simple stress boxes

--- public/js/charsheet.js
+++ public/js/charsheet.js
@@ -3,6 +3,21 @@ function initialize()
 {
 	function processCharacterData(data,textStatus,jqXHR)
 	{
+		for( var i in data.stress ){
+			data.stress[i].boxes = ko.computed(function(){
+				var boxes = [];
+				for( var j=1; j<=8; j++ ){
+					var box = data.stress[i];
+					boxes.push({
+						'checked': box.used.indexOf(j) != -1,
+						'enabled': j <= box.strength + box.toughness,
+						'left-paren': box.toughness > 0 && j == box.strength+1 ? 'left-paren' : 'normal',
+						'right-paren': box.toughness > 0 && j == box.strength+box.toughness ? 'right-paren' : 'normal'
+					});
+				}
+				return boxes;
+			});
+		}
 		ko.applyBindings(data);
 	}
 
