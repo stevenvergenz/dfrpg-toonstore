5b3244cde61ca9a3782dd5b8b5d41ed48d638596
Steven Vergenz <vergenzs@gmail.com>
2013-07-02 10:26:03 -0400

    Added consequences and totals box to knockout sheet

--- public/js/charsheet.js
+++ public/js/charsheet.js
@@ -3,7 +3,9 @@ function initialize()
 {
 	function processCharacterData(data,textStatus,jqXHR)
 	{
-		for( var i in data.stress ){
+		// add an itemized format for stress boxes
+		for( var i in data.stress )
+		{
 			var track = data.stress[i];
 			track.boxes = ko.computed(function(){
 				var boxes = [];
@@ -23,7 +25,10 @@ function initialize()
 				}
 				return boxes;
 			});
-			for( var j in track.armor ){
+
+			// add an itemized format for armor boxes
+			for( var j in track.armor )
+			{
 				var armor = track.armor[j];
 				armor.boxes = ko.computed(function(){
 					var boxes = [];
@@ -37,6 +42,34 @@ function initialize()
 				});
 			}
 		}
+
+		// calculate consequence strength
+		for( var i in data.consequences ){
+			var conseq = data.consequences[i];
+			conseq.strength = ko.computed(function(){
+				if( this.type == 'Mild' )
+					return -2;
+				else if( this.type == 'Moderate' )
+					return -4;
+				else if( this.type == 'Severe' )
+					return -6;
+				else if( this.type == 'Extreme' )
+					return -8;
+				else
+					return 0;
+			}, conseq);
+		}
+
+		data.totals.skill_cap_text = ko.computed(function(){
+			var ladder = ['Mediocre (+0)', 'Average (+1)', 'Fair (+2)', 'Good (+3)', 'Great (+4)', 'Superb (+5)', 'Fantastic (+6)', 'Epic (+7)', 'Legendary (+8)'];
+			return ladder[this.skill_cap];
+		}, data.totals);
+
+		data.totals.skills_spent = ko.computed(function(){
+			return this.skills_total - this.skills_available;
+		}, data.totals);
+
+		// apply modified json document to page
 		ko.applyBindings(data);
 	}
 
