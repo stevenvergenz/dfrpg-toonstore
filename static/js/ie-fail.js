b2319956b522cc5e7b3d6dcf0607bcda9162c79b
Steven Vergenz <vergenzs@gmail.com>
2014-03-06 12:29:30 -0500

    Broadened warning for unsupported browsers

--- static/js/ie-fail.js
+++ static/js/ie-fail.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	if( !Modernizr.flexbox ){
-		$('.content').prepend( $('<div class="content-block"><div class="warning"><p>This page is not fully supported by your browser. If you experience problems, try upgrading to the latest version or switching to Chrome.</p></div></div>') );
+		$('.content').prepend( $('<div class="content-block"><div class="warning"><p>This site is <a href="http://caniuse.com/flexbox" style="color:blue;">not fully supported</a> by your browser. If you experience problems, try upgrading to the latest version of your browser, or switching to Chrome.</p></div></div>') );
 	}
 });
