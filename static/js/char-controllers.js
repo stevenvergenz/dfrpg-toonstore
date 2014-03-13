1683f9eddaed54cfde22dbfbe66ab94a7aadc64b
Steven Vergenz <vergenzs@gmail.com>
2014-03-13 17:00:48 -0400

    Added droppable, finished skills panel

--- static/js/char-controllers.js
+++ static/js/char-controllers.js
@@ -29,9 +29,8 @@ app.directive('dgyDraggable', function()
 		'link': function(scope,element,attr)
 		{
 			// initialize
-			console.log('init false');
 			element.draggable({
-				'revert': 'invalid',
+				'revert': true,
 				'disabled': !scope.enabled()
 			});
 
@@ -41,7 +40,7 @@ app.directive('dgyDraggable', function()
 			});
 
 			// cleanup
-			element.on('$destroy', function(){
+			element.bind('$destroy', function(){
 				element.draggable('destroy');
 			});
 		}
@@ -53,11 +52,26 @@ app.directive('dgyDroppable', function()
 	return {
 		'restrict': 'A',
 		'scope': {
-			'enabled': '&dgyDroppable'
+			'enabled': '&dgyDroppable',
+			'callback': '&dgyDrop'
 		},
 		'link': function(scope,element,attr)
 		{
-			
+			// initialize
+			element.droppable({
+				'disabled': !scope.enabled(),
+				'hoverClass': 'hoverDropTarget',
+				'drop': scope.callback()
+			});
+
+			scope.$watch(scope.enabled, function(){
+				element.droppable('option', 'disabled', !scope.enabled());
+			});
+
+			// cleanup
+			element.bind('$destroy', function(){
+				element.droppable('destroy');
+			});
 		}
 	};
 });
@@ -103,13 +117,13 @@ function SkillCtrl($scope, rootModel)
 	$scope.editing = false;
 	$scope.shifted = false;
 
-	// data.skills.lists
-	// data.skills.shifted_lists
+	// convenience label function
 	$scope.label = function(value){
 		var ladder = ['Mediocre (+0)', 'Average (+1)', 'Fair (+2)', 'Good (+3)', 'Great (+4)', 'Superb (+5)', 'Fantastic (+6)', 'Epic (+7)', 'Legendary (+8)'];
 		return ladder[value];
 	};
 
+	// correct ordering of skill groups
 	$scope.presOrder = function(){
 		var arr = [];
 		if( ! $scope.data.$resolved )
@@ -121,6 +135,7 @@ function SkillCtrl($scope, rootModel)
 		return arr;
 	};
 
+	// select correct list
 	$scope.skills = function(level)
 	{
 		if( $scope.shifted )
@@ -128,13 +143,66 @@ function SkillCtrl($scope, rootModel)
 		else
 			return $scope.data.skills.lists[level];
 	};
+
+	// add new skill
+	$scope.addSkill = function(skillName)
+	{
+		$scope.skills(0).push(skillName);
+		$scope.skills(0).sort();
+	};
+
+	// drag-drop handler
+	$scope.dropHandler = function(evt, ui)
+	{
+		var skill = ui.draggable.find('span').first().text();
+		var draggedLevel = angular.element(ui.draggable).parent().scope().level;
+		var droppedLevel = angular.element(evt.target).scope().level;
+		
+		if( draggedLevel != droppedLevel )
+		{
+			// remove from old group
+			var pos = $scope.skills(draggedLevel).indexOf(skill);
+			$scope.skills(draggedLevel).splice(pos,1);
+
+			// add to new group (if not removing)
+			if( droppedLevel != 0 ){
+				$scope.skills(droppedLevel).push(skill);
+				$scope.skills(droppedLevel).sort();
+			}
+
+			// update ui
+			$scope.$apply();
+
+			console.log('Moved skill from', draggedLevel, 'to', droppedLevel);
+		}
+	};
+
+	// calculate skill points available
+	$scope.pointsAvailable = function(){
+		var spent = 0;
+		for(var i=0; i<8; i++){
+			spent += i * $scope.skills(i).length;
+		}
+		return $scope.data.totals.skills_total - spent;
+	};
+
+	// skill ladder validator
+	$scope.valid = function(){
+		var valid = true;
+		if( !$scope.data.$resolved )
+			return valid;
+
+		for( var i=1; i<$scope.data.totals.skill_cap; i++ ){
+			valid &= $scope.skills(i).length >= $scope.skills(i+1).length;
+		}
+		return (valid ? 'Valid' : 'INVALID') + ', '+$scope.pointsAvailable()+' available';
+	};
 }
 
 
 // special global-scope function to upload avatar
 function uploadAvatar()
 {
-	console.log('stuff');
 	var formData = new FormData($('form')[0]);
 	$.ajax({
 		'url': 'avatar',
